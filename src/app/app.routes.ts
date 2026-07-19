e82b27369fb49c0524759eaa1b6c6853254c1b01
Bhanu Prasad <bhanuprasadtarra@gmail.com>
2026-07-19 22:40:46 +0200

    Define application routes for ListPage and DetailsPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListPage } from './pages/list/list.page';
+import { DetailsPage } from './pages/details/details.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: 'list', component: ListPage },
+  { path: 'details', component: DetailsPage },
+];
